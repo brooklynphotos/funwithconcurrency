6c6957a623873dd45b639c10ef5c7b6e1cb9553c
Brooklyn Photos <guoneng.list@gmail.com>
2019-11-12 23:15:08 +0100

    initial sequential servers

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
 	helloWorldSvr := getHellowWorldServer()
+	helloNameSvr := getHelloNameServer()
+	echoSvr := getEchoServer()
 
 	helloWorldSvr.ListenAndServe()
+	helloNameSvr.ListenAndServe()
+	echoSvr.ListenAndServe()
+	fmt.Println("all servers are started")
 }
 
 func getHellowWorldServer() *http.Server {
@@ -16,4 +25,27 @@ func getHellowWorldServer() *http.Server {
 	})
 
 	return &http.Server{Addr: ":7000", Handler: mux}
+}
+
+func getHelloNameServer() *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		params := r.URL.Query()
+		name := params.Get("name")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
+	})
+
+	return &http.Server{Addr: ":8000", Handler: mux}
+}
+
+func getEchoServer() *http.Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		io.Copy(w, r.Body)
+	})
+
+	return &http.Server{Addr: ":9000", Handler: mux}
 }
